29738b6777c77713440c6b482d1f581b02265ef7
Iryna Kresinska <kresi.ira@gmail.com>
2021-10-28 18:57:46 +0200

    useState
    toggle button

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,19 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [value, setValue] = useState(true);
+  return (
+      <>
+        <h2>{value ? "YES" : "NO"}</h2>
+        <button
+            type="button"
+            className="btn"
+            onClick={() => {setValue(prevState => !prevState)}}
+        >
+         Toggle Yes/No
+        </button>
+      </>
+  )
 };
 
 export default UseStateBasics;
