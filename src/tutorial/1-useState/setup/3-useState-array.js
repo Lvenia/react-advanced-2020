06038e013af52680891dc1b254fdde5f621dfc95
Iryna Kresinska <kresi.ira@gmail.com>
2021-10-28 19:51:29 +0200

    useState
    render list, remove list, remove items

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+      const newList = people.filter((el) => el.id !== id);
+      setPeople(newList);
+  }
+  return (
+      <>
+        { people.map(person => {
+          const {id, name} = person;
+          return (
+              <div className="item" key={id}>
+                <h4>{name}</h4>
+                <button
+                    type="button"
+                    className="btn"
+                    onClick={() => removeItem(id)}
+                >
+                    Remove item
+                </button>
+              </div>
+          )
+        })}
+        <button
+            type="button"
+            className="btn"
+            onClick={() => setPeople([])}
+        >
+            Remove all items
+        </button>
+      </>
+  );
 };
 
 export default UseStateArray;
