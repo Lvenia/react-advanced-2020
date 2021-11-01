e8299a61b896cb128148689a6c02d01c88778c12
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-01 19:44:02 +0100

    useEffect
    run effect only on provided variable update

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {findByDisplayValue} from "@testing-library/react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
@@ -9,7 +10,7 @@ const UseEffectBasics = () => {
     if (count > 0) {
       document.title = `New messages(${count})`;
     }
-  }, []);
+  }, [count]);
 
   return (
       <>
