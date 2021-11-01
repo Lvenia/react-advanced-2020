52a887d76110e04465ed4024dcbf25c142581760
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-01 20:14:36 +0100

    useEffect
    second argument and cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,33 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  }
+
+  //using cleanup considered as a good practice,
+  // even though in this particular case it would be enough
+  // to provide second argument in order to set listener just once
+  // with the first render
+  useEffect(() => {
+      window.addEventListener('resize', handleResize);
+      return () => {
+          window.removeEventListener('resize',handleResize)
+      }
+  })
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize);
+  // }, [])
+
+  return (
+      <>
+        <h2>Screen width</h2>
+        <h3>{width}</h3>
+      </>
+  )
 };
 
 export default UseEffectCleanup;
