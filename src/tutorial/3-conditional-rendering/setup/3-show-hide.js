31440b47b33d27d7104863c428ba8d033753ee69
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-02 15:24:41 +0100

    event listener on the toggled elemen

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+      <>
+        <button
+            type="button"
+            className="btn"
+            onClick={() => setShow(!show)}
+        >
+          Toggle Screen Width
+        </button>
+        {show && <Item/>}
+      </>
+  )
 };
+
+const Item = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
+  return (
+      <div>
+        <h2>Screen Width</h2>
+        <h3>{screenWidth}</h3>
+      </div>
+  )
+}
 
 export default ShowHide;
