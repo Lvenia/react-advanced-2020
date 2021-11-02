fa8ae8d473b5f897afcc02b4ffff59bf7dad466e
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-02 15:07:58 +0100

    short circuit evaluation, conditional rendering

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    // isError && <h2>There is an error</h2> //Logical AND short circuit evaluation, isError true => h2 is shown
+    // isError || <h2>There is an error</h2> //Logical OR short circuit evaluation, isError false => h2 is shown
+    <h2>{isError ? "There is an Error" : "Content"}</h2>
+  )
 };
 
 export default ShortCircuit;
