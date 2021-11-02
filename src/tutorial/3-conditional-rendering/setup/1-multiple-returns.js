63d3fa20ca14a3d55e9f8ab4df0282a7a5f34358
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-02 12:44:08 +0100

    useEffect
    multiple returns, github user card project: fetch, handle loading and error

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,48 @@
 import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+const url = 'https://api.github.com/users/Lvenia';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(url)
+        .then(response => {
+          setIsLoading(false);
+          if(response.status === 200) {
+            return response.json();
+          } else {
+            throw new Error(response.statusText)
+          }
+        })
+        .then(user => {
+          setUser(user);
+        })
+        .catch(err => {
+          setIsError(true);
+          console.log(err);
+        })
+  }, []);
+
+  if(isLoading) {
+    return <h2>Loading...</h2>
+  }
+
+  if(isError) {
+    return <h2>Error</h2>
+  }
+
+  const {avatar_url, login, html_url} = user;
+  return (
+      <div className="card">
+        <img src={avatar_url} alt={login}/>
+        <div>
+          <h3>{login}</h3>
+          <a href={html_url}>Profile</a>
+        </div>
+      </div>
+  )
 };
 
 export default MultipleReturns;
