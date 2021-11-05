3a00693ee8e1e44bc88ba16976c76913f4848df0
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-05 15:51:43 +0100

    useReducer controlled form with useState

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,36 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [list, setList] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  const [item, setItem] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(item){
+      setList([...list, {id: new Date().getTime().toString(), name: item}]);
+      setItem("");
+    }
+    setShowModal(true);
+    setTimeout(() => {
+      setShowModal(false);
+    }, 1000)
+  }
+
+  const handleChange = (e) => {
+    setItem(e.target.value)
+  }
+  return (
+      <>
+        {showModal && <Modal/>}
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" value={item} onChange={handleChange}/>
+          <button type="submit">Add item</button>
+        </form>
+        {list.map(item => {
+          return <h4 key={item.id}>{item.name}</h4>
+        })}
+      </>
+  )
 };
 
 export default Index;
