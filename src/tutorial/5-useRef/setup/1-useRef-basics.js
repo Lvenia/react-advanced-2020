7e82ba8856c57a54d906ab4d8d072af412d6e1aa
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-05 12:00:28 +0100

    useRef
    target DOM element, target its value

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,28 @@
 import React, { useEffect, useRef } from 'react';
-
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+
+  useEffect(() => {
+    refContainer.current.focus();
+  }) //useRef does not trigger re-render
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value)
+  }
+  return (
+      <>
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" ref={refContainer}/>
+          <button type="submit">Submit</button>
+        </form>
+      </>
+  )
 };
 
 export default UseRefBasics;
+
