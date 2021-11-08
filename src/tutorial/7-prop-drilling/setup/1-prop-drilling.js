d703375b4e829c497cb3f560ca3a8692986edafe
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-08 16:49:26 +0100

    prop drilling example

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
-
-// more components
-// fix - context api, redux (for more complex cases)
+import {data} from '../../../data';
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [list, setList] = useState(data);
+
+  const handleRemove = (id) => {
+    setList(list.filter(item => item.id !== id));
+  };
+
+  return (
+      <section>
+        <h3>Prop Drilling</h3>
+        <List people={list} handleRemove = {handleRemove}/>
+      </section>
+  )
 };
 
+//List component does not use handleRemove function, it only passes it to the child
+const List = ({people, handleRemove}) => {
+  return (
+      <>
+        {people.map(item => {
+          const {id, name} = item;
+          return <Person key={id} id={id} name={name} handleRemove={handleRemove}/>
+        })}
+      </>
+  )
+}
+
+const Person = ({id, name, handleRemove}) => {
+  return (
+      <div className="item">
+        <h4>{name}</h4>
+        <button onClick={() => handleRemove(id)}>remove</button>
+      </div>
+  )
+}
 export default PropDrilling;
