95c308447f8b311cda5afd21898b43b14738eec6
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-09 11:41:22 +0100

    custom hooks - useFetch
    move fetch functionality in a separate module to reuse it

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,19 @@
 import { useState, useEffect } from 'react';
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+    const [loading, setLoading] = useState(false);
+    const [result, setResult] = useState([]);
+
+    const getData = async () => {
+        setLoading(true);
+        const response = await fetch(url);
+        const result = await response.json();
+        setResult(result);
+        setLoading(false);
+    }
+    useEffect(() => {
+        getData();
+    }, [url]);
+
+    return {loading, result};
+};
