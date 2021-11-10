1c2249cab4a0ec1de546c0f15c8a73891fad770d
Iryna Kresinska <kresi.ira@gmail.com>
2021-11-10 13:51:58 +0100

    PropTypes
    set propTypes and defaultTypes properties on the react component to ensure proper rendering and prevent errors caused by undefined values of component's properties

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({name, image, price}) => {
+  // if default image is not provided in defaultProps property of component:
+  // assign value of the url to a separate variable
+  // use short-circuit evaluation in JSX
+  // const imageUrl = image && image.url;
+  return (
+      <article className='product'>
+        {/*<img src={imageUrl || defaultImage } alt={name}/>*/}
+          <img src={image.url} alt={name}/>
+        <h4>{name}</h4>
+        <p>${price}</p>
+      </article>
+  );
 };
 
+Product.propsType = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+    price: " not provided",
+    image: {
+        url: defaultImage,
+    }
+}
 export default Product;
